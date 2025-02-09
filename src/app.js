1eb30b46264b178376ef3b93504521add991932a
MITRAJ <rajatmittalofficial@gmail.com>
2025-02-09 08:43:01 +0530

    Base url 4

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ require("./utils/cronjob");
 
 app.use(
   cors({
-    origin: "https://devtinder-frontend-three.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
